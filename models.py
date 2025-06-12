63112bfdf9b1d64068d40ecacc344fdf6f2a5641
labsis <kssoto29@ucatolica.edu.co>
2025-06-12 03:54:27 -0500

    add disponibles

--- models.py
+++ models.py
@@ -4,15 +4,16 @@ from sqlmodel import SQLModel, Field
 class Vuelos (SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     origen: str = Field(index=True, unique=True)
+    disponible: bool = Field(default=True)
     destino: str
     fecha: float
-    pagado: bool
+    pagado: bool = Field(default=True)
 
 class Usuario(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str = Field(index=True, unique=True)
     reservas: str
-    mascota:bool
+    mascota:bool = Field(default=True)
 
 class Mascotas(SQLModel, table=True):
     id_usuario: int=Field(default=None, primary_key=True)
